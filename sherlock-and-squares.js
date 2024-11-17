d546e4aab28ff8ea0e5ea133a581c421b2be8d84
Broderick <zerothesu@gmail.com>
2024-11-16 20:07:27 -0700

    reset hackerrank challenge sherlock

--- sherlock-and-squares.js
+++ sherlock-and-squares.js
@@ -1,11 +1,2 @@
 function squares(a,b,c=0) {
-    if(b === a) {
-        return c
-    }
-    if(Math.sqrt(b) === Math.floor(Math.sqrt(b))){
-        c++
-    }
-    return squares(a, b-1,c=c)
 }
-let cringe = squares(100,1000)
-console.log(cringe)
